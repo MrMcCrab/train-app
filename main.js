7b05092bcddbdb5351ac418dea283c38a42299ee
MrMcCrab <>
2017-12-14 14:11:15 +0200

    Comments and time format fix

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-function getCommuterTrains(stationCode) {
+
+// Function requests data for all trains for current day
+function getTrainData(){
 
   var url = "https://rata.digitraffic.fi/api/v1/trains/" + getToday();
-  console.log("get " + stationCode);
 
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.open("GET", url, true);
@@ -10,77 +11,92 @@ function getCommuterTrains(stationCode) {
   xmlhttp.onreadystatechange = function(){
 
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      // Data is placd in a global variable to make it easily accessible by other functions
+      trainData = JSON.parse(xmlhttp.responseText);
+      console.log("got train data");
+      // Once the data is received the value of the first option is changed
+      // and the dropdown is enabled
+      $("#firstOption").html("Select station");
+      $("#dropdown").prop("disabled", false);
 
-      $("select").html=("Select station");
-      var data = JSON.parse(xmlhttp.responseText);
-
-      table = "<h3>" + "Commuter trains from " + $("#dropdown").val() + "</h3>";
-      table += "<table class='table'>";
-
-      table += "<thead>";
-      table += "<tr>";
-      table += "<th>" + "Destination" + "</th>";
-      table += "<th>" + "Train" + "</th>";
-      table += "<th>" + "Track" + "</th>";
-      table += "<th>" + "Departure" + "</th>";
-      table += "</tr>";
-      table += "</thead>";
-
-      for (var i = 0; i < data.length; i++) {
-          for (var j = 0; j < data[i].timeTableRows.length; j++) {
-            if (data[i].timeTableRows[j].stationShortCode == stationCode && data[i].timeTableRows[j].trainStopping == true && data[i].trainType == "HL" && data[i].timeTableRows[j].type == "DEPARTURE") {
-              console.log(data[i]);
-
-                table += "<tr>";
-
-                table += "<td>" + getDestination(data[i].timeTableRows[data[i].timeTableRows.length-1].stationShortCode) + "</td>";
-                table += "<td>" + data[i].commuterLineID + "</td>";
-                table += "<td>" + data[i].timeTableRows[j].commercialTrack + "</td>";
-                table += "<td>" + getTime(data[i].timeTableRows[j].scheduledTime) + "</td>";
-
-                table += "</tr>";
-
-            }
-          }
-          // console.log(data[i]);
-        // }
-      }
-      // console.log(data)
-      table += "</table>";
-      $("#table").html(table);
     }
   }
 }
 
 
+// function is used to select data matching the selected station
+function getCommuterTrains(stationCode) {
 
+  // Get the station name from the dropdown
+  var table = "<h3>" + "Trains from " + $("#dropdown").val() + "</h3>";
+  table += "<table class='table'>";
+
+  table += "<thead>";
+  table += "<tr>";
+  table += "<th>" + "Destination" + "</th>";
+  table += "<th>" + "Train" + "</th>";
+  table += "<th>" + "Track" + "</th>";
+  table += "<th>" + "Departure" + "</th>";
+  table += "</tr>";
+  table += "</thead>";
+
+  // Iterate through the data and find time table entries for stations where the train stops and time table type is departure
+  for (var i = 0; i < trainData.length; i++) {
+    for (var j = 0; j < trainData[i].timeTableRows.length; j++) {
+      if (trainData[i].timeTableRows[j].stationShortCode == stationCode && trainData[i].timeTableRows[j].trainStopping == true &&  trainData[i].timeTableRows[j].type == "DEPARTURE") {
+        console.log(trainData[i]);
+
+          table += "<tr>";
+          // Finds the station name using the station short code
+          table += "<td>" + getDestination(trainData[i].timeTableRows[trainData[i].timeTableRows.length-1].stationShortCode) + "</td>";
+          // Check if commuter line ID is empty, if it is use train type and train number
+          if (trainData[i].commuterLineID == "") {
+            table += "<td>" + trainData[i].trainType + trainData[i].trainNumber + "</td>";
+          }else {
+            table += "<td>" + trainData[i].commuterLineID + "</td>";
+          }
+          // Checks if the train is cancelled
+          if (trainData[i].cancelled == true) {
+            table += "<td>" + "cancelled" + "</td>";
+          }else {
+            table += "<td>" + trainData[i].timeTableRows[j].commercialTrack + "</td>";
+          }
+          // Function return the departure time
+          table += "<td>" + getTime(trainData[i].timeTableRows[j].scheduledTime) + "</td>";
+
+
+          table += "</tr>";
+
+      }
+    }
+  }
+  table += "</table>";
+  $("#table").html(table);
+}
+
+
+// Function returns the station name matching the received short code
 function getDestination(id){
   for (var i = 0; i < stationList.length; i++) {
     if (stationList[i].stationShortCode == id){
-        return stationList[i].stationName;
+      return stationList[i].stationName;
     }
   }
 }
 
 
 
-function getCurrentHour(){
-  var time = new Date();
-  var currentHour = time.toISOString();
-  currentHour = currentHour.substr(11, 5);
-  console.log(currentHour);
-}
-
-
-
+// Function returns the departure time of the train from the received date
 function getTime(time) {
-  // 2017-12-09T22:05:00.000Z
-  let hours = time.substring(11, 16);
-  return hours;
+  var newTime = new Date(time);
+  var hours = newTime.toString();
+  var departureTime = hours.substring(16, 21);
+  return departureTime;
 }
 
 
-
+// Function is used to get the current year, month and day and return it in
+// correct format when requesting train data
 function getToday(){
   var year = new Date();
   year = year.getFullYear();
@@ -103,8 +119,10 @@ function getToday(){
 }
 
 
-
+// Function requests the list of stations
 function getStationList(){
+  // Disable the dropdown to prevent the app from crashing if the station is selected before trein data is received
+  $("#dropdown").prop("disabled", true);
 
   var url = "https://rata.digitraffic.fi/api/v1/metadata/stations";
   var xmlhttp = new XMLHttpRequest();
@@ -115,27 +133,28 @@ function getStationList(){
   xmlhttp.onreadystatechange = function(){
     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
       stationList = JSON.parse(xmlhttp.responseText);
-
+      // Crete a new option for each station
+      // Uncommenting the if statement lists only passenger stations
       for (var i = 0; i < stationList.length; i++) {
-        if (stationList[i].passengerTraffic == true) {
+        // if (stationList[i].passengerTraffic == true) {
           var option = document.createElement("option");
           option.setAttribute("value", stationList[i].stationName);
           option.appendChild(document.createTextNode(stationList[i].stationName));
           dropdown.appendChild(option);
-        }
+        // }
       }
     }
   }
 }
 
 
-
+// Function gets the station short code from the station list and then
+// Calls the getCommuterTrains function with the code
 function getStationCode(){
 
   var input = $("#dropdown").val();
   console.log(input);
 
-  // console.log(stationList);
   for(var i = 0; i < stationList.length; i++){
     if (stationList[i].stationName == input) {
       console.log(stationList[i].stationShortCode);
